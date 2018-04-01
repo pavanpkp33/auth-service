e38e0502db5366b38d86529b7405944ce2f2d492
pavanpkp33 <pavanpkp33@gmail.com>
2018-03-31 21:52:20 -0700

    bug fixes

--- src/main/java/com/sdsu/edu/cms/authservice/AuthServiceApplication.java
+++ src/main/java/com/sdsu/edu/cms/authservice/AuthServiceApplication.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import java.awt.*;
 
 @SpringBootApplication
 @EnableDiscoveryClient
---
+++ src/main/java/com/sdsu/edu/cms/authservice/service/AuthService.java
@@ -0,0 +1,21 @@
+package com.sdsu.edu.cms.authservice.service;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AuthService {
+
+
+    public boolean verifyPassword(String userPwd, String dbPwd){
+        if(userPwd.isEmpty() || dbPwd.isEmpty()) return false;
+       return BCrypt.checkpw(userPwd, dbPwd);
+    }
+
+    public String hashPwd(String userPwd){
+        if(userPwd.isEmpty()) return null;
+
+        return BCrypt.hashpw(userPwd, BCrypt.gensalt());
+    }
+}
